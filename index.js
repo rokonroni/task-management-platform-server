c389e45bee5675ef9e1c2f29ff1b6acb66875e52
rokonroni <rokonuzzamanroni@gmail.com>
2023-12-22 12:30:12 +0600

    Get task and patch task added

--- index.js
+++ index.js
@@ -5,15 +5,14 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(express.json());
 
 const {
   MongoClient,
   ServerApiVersion,
-  ObjectId,
   CURSOR_FLAGS,
+  ObjectId,
 } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.8sb7n8j.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -52,13 +51,65 @@ async function run() {
       });
     };
 
+    app.get("/getToDoTask", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const taskType = req.query.taskType || "to-do"; 
+      const query = { userEmail: email, taskType: taskType };
+      const result = await taskCollection
+        .find(query)
+        .sort({ taskAddDate: -1 })
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/getOngoingTask", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const taskType = req.query.taskType || "ongoing"; 
+      const query = { userEmail: email, taskType: taskType };
+      const result = await taskCollection
+        .find(query)
+        .sort({ taskAddDate: -1 })
+        .toArray();
+      res.send(result);
+    });
+
+    app.get("/getCompleteTask", verifyToken, async (req, res) => {
+      const email = req.query.email;
+      const taskType = req.query.taskType || "complete";
+      const query = { userEmail: email, taskType: taskType };
+      const result = await taskCollection
+        .find(query)
+        .sort({ taskAddDate: -1 })
+        .toArray();
+      res.send(result);
+    });
+
+    app.patch('/updateTaskType/:taskId', verifyToken, async (req, res) => {
+  const taskId = req.params.taskId;
+  const { taskType } = req.body;
+
+  try {
+    const result = await taskCollection.updateOne(
+      { _id: new ObjectId(taskId) },
+      { $set: { taskType: taskType } }
+    );
+
+    if (result.modifiedCount === 1) {
+      res.status(200).send({ message: 'TaskType updated successfully' });
+    } else {
+      res.status(404).send({ message: 'Task not found' });
+    }
+  } catch (error) {
+    console.error('Error updating task type:', error);
+    res.status(500).send({ message: 'Internal Server Error' });
+  }
+});
+
     app.post("/tasks", async (req, res) => {
       const results = req.body;
       const result = await taskCollection.insertOne(results);
       res.send(result);
     });
-
-
 
     await client.db("admin").command({ ping: 1 });
     console.log(
